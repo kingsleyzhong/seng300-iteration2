03a0420f3cdaf4b193fb88e11c3d1f089f70fa91
anthony kostal-vazquez <anthonykostal@gmail.com>
2023-11-14 13:30:22 -0700

    added comments for test suite

---
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/RemoveItemTests.java
@@ -0,0 +1,20 @@
+package com.thelocalmarketplace.software.test;
+
+public class RemoveItemTests {
+	//Successfully remove item (update weight and price)
+	
+	
+	//remove item that hasn't been scanned
+	
+	
+	
+	//remove duplicate item (update weight and price)
+	
+	
+	
+	//remove item twice
+	
+	
+	
+	
+}
