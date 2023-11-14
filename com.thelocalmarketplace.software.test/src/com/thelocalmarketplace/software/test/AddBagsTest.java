1654de13c7d660dc5c4ef7aa8b9b9b1e7109fa39
anthony kostal-vazquez <anthonykostal@gmail.com>
2023-11-14 11:42:47 -0700

    commentend the test cases that we need

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/AddBagsTest.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/AddBagsTest.java
@@ -1,5 +1,24 @@
 package com.thelocalmarketplace.software.test;
 
 public class AddBagsTest {
-
+	//Customer indicates they want to add bag, adds bag then system detects weight change
+	
+	
+	//Customer indicates they want to add bag then adds nothing
+	
+	
+	//customer indicates they want to add bag then removes something from scale
+	
+	
+	//Customer indicates they want to add a bag then adds something heavy ()
+	
+	
+	//customer indicates they want to add bag then cancels
+	
+	
+	
+	
+	
+	
+	
 }
