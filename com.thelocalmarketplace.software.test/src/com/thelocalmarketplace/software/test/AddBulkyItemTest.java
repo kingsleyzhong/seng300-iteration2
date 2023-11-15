60623dabab86b837aa0718a3913d5606a1c8b28b
Sua <sua.lim@ucalgary.ca>
2023-11-14 21:31:39 -0700

    More test cases

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/AddBulkyItemTest.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/AddBulkyItemTest.java
@@ -1,6 +1,11 @@
 package com.thelocalmarketplace.software.test;
 
 import static org.junit.Assert.*;
+
+import com.jjjwelectronics.IDevice;
+import com.jjjwelectronics.IDeviceListener;
+import com.jjjwelectronics.scanner.BarcodeScannerListener;
+import com.jjjwelectronics.scanner.IBarcodeScanner;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -20,6 +25,7 @@ import com.thelocalmarketplace.software.weight.Weight;
 
 import powerutility.PowerGrid;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -49,8 +55,14 @@ public class AddBulkyItemTest {
     private SelfCheckoutStationGold scsg;
 
     private Session session;
+    private Session sessionBronze;
+    private Session sessionSilver;
+    private Session sessionGold;
     private BarcodedProduct product;
     private BarcodedProduct product2;
+    private BarcodedItem item;
+    private ScannerListenerStub listener;
+
     byte num;
     private Numeral numeral;
     private Numeral[] digits;
@@ -59,6 +71,7 @@ public class AddBulkyItemTest {
 
     private Funds funds;
     private Weight weight;
+    private Weight weightBronze;
     private Weight weightSilver;
     private Weight weightGold;
 
@@ -70,7 +83,16 @@ public class AddBulkyItemTest {
         scss = new SelfCheckoutStationSilver();
         scsg = new SelfCheckoutStationGold();
 
+        listener = new ScannerListenerStub();
+        scs.handheldScanner.register(listener);
+        scss.handheldScanner.register(listener);
+        scsg.handheldScanner.register(listener);
+
         session = new Session();
+        sessionBronze = new Session();
+        sessionSilver = new Session();
+        sessionGold = new Session();
+
         num = 1;
         numeral = Numeral.valueOf(num);
         digits = new Numeral[] { numeral, numeral, numeral };
@@ -79,7 +101,12 @@ public class AddBulkyItemTest {
         product = new BarcodedProduct(barcode, "Sample Product", 10, 100.0);
         product2 = new BarcodedProduct(barcode2, "Sample Product 2", 15, 20.0);
         funds = new Funds(scs);
+        item = new BarcodedItem(barcode, new Mass(100.0));
+
         weight = new Weight(scs);
+        weightBronze = new Weight(scs);
+        weightSilver = new Weight(scss);
+        weightGold = new Weight(scsg);
     }
 
     /**
@@ -100,6 +127,78 @@ public class AddBulkyItemTest {
         Mass expected = new Mass(0);
         assertEquals("Mass is 0", expected, actual);
     }
+
+    /**
+     * test case for adding bulky item by handheld scan bronze
+     * Scenario: scan an item with handheld scanner, call addBulkyItem
+     */
+    @Test
+    public void testAddBulkyItemBronzeHandheldScanner() {
+        scs.plugIn(PowerGrid.instance());
+        scs.turnOn();
+
+        session.start();
+        session.setup(new HashMap<BarcodedProduct, Integer>(), funds, weightBronze);
+
+        while (!listener.barcodesScanned.contains(item.getBarcode())) {
+            scs.handheldScanner.scan(item);
+        }
+
+        session.addBulkyItem();
+        Weight itemWeight = session.getWeight();
+        Mass actual = itemWeight.getExpectedWeight();
+        Mass expected = new Mass(0);
+        assertEquals("Mass is 0", expected, actual);
+    }
+
+    /**
+     * test case for adding bulky item by handheld scan silver
+     * Scenario: scan an item with handheld scanner, call addBulkyItem
+     */
+    @Test
+    public void testAddBulkyItemSilverHandheldScanner() {
+        scss.plugIn(PowerGrid.instance());
+        scss.turnOn();
+
+        session.start();
+        session.setup(new HashMap<BarcodedProduct, Integer>(), funds, weightSilver);
+
+        while (!listener.barcodesScanned.contains(item.getBarcode())) {
+            scss.handheldScanner.scan(item); // mass is 100
+        }
+
+        session.addBulkyItem();     // mass should now be 0
+
+        Weight itemWeight = session.getWeight();
+        Mass actual = itemWeight.getExpectedWeight();
+        Mass expected = new Mass(0);
+        assertEquals("Mass is 0", expected, actual);
+    }
+
+    /**
+     * test case for adding bulky item by handheld scan gold
+     * Scenario: scan an item with handheld scanner, call addBulkyItem
+     */
+    @Test
+    public void testAddBulkyItemGoldHandheldScanner() {
+        scsg.plugIn(PowerGrid.instance());
+        scsg.turnOn();
+
+        session.start();
+        session.setup(new HashMap<BarcodedProduct, Integer>(), funds, weightGold);
+
+        while (!listener.barcodesScanned.contains(item.getBarcode())) {
+            scsg.handheldScanner.scan(item); // mass is 100
+        }
+
+        session.addBulkyItem();     // mass should now be 0
+
+        Weight itemWeight = session.getWeight();
+        Mass actual = itemWeight.getExpectedWeight();
+        Mass expected = new Mass(0);
+        assertEquals("Mass is 0", expected, actual);
+    }
+
 
     /**
      * test case for checking if the session is IN_SESSION if adding bulky item
@@ -210,5 +309,45 @@ public class AddBulkyItemTest {
         scs.baggingArea.addAnItem(item);
         session.addBulkyItem();
         assertFalse("Cannot call AddBulkyItem if there is no weight discrepancy", session.getCallAddBulkyItem());
+    }
+
+
+
+    public class ScannerListenerStub implements BarcodeScannerListener {
+        public ArrayList<Barcode> barcodesScanned;
+
+        ScannerListenerStub(){
+            barcodesScanned = new ArrayList<Barcode>();
+        }
+
+        @Override
+        public void aDeviceHasBeenEnabled(IDevice<? extends IDeviceListener> device) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void aDeviceHasBeenDisabled(IDevice<? extends IDeviceListener> device) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void aDeviceHasBeenTurnedOn(IDevice<? extends IDeviceListener> device) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void aDeviceHasBeenTurnedOff(IDevice<? extends IDeviceListener> device) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void aBarcodeHasBeenScanned(IBarcodeScanner barcodeScanner, Barcode barcode) {
+            barcodesScanned.add(barcode);
+        }
+
     }
 }
