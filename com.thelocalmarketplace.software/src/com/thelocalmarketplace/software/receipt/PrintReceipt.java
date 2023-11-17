ec4dc1e18ab28dbbc6a6cbfffebc12c36ec17d4a
CeeRobitaille <chloe.robitaille@ucalgary.ca>
2023-11-17 16:27:22 -0700

    Added the header with names and UCIDs to a couple files that didnt have them.

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/receipt/PrintReceipt.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/receipt/PrintReceipt.java
@@ -9,7 +9,20 @@ import com.jjjwelectronics.OverloadedDevice;
 import com.jjjwelectronics.printer.IReceiptPrinter;
 import com.jjjwelectronics.printer.ReceiptPrinterListener;
 import com.thelocalmarketplace.hardware.AbstractSelfCheckoutStation;
-
+/*
+ * 
+ * Project iteration 2 group members:
+ * Aj Sallh : 30023811
+ * Anthony Kostal-Vazquez : 30048301
+ * Chloe Robitaille : 30022887
+ * Dvij Raval : 30024340
+ * Emily Kiddle : 30122331
+ * Katelan NG : 30144672
+ * Kingsley Zhong : 30197260
+ * Nick McCamis : 30192610
+ * Sua Lim : 30177039
+ * Subeg CHAHAL : 30196531
+ */
 public class PrintReceipt {
 	
 	public ArrayList<PrintReceiptListener> listeners = new ArrayList<>();
--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/receipt/PrintReceiptListener.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/receipt/PrintReceiptListener.java
@@ -1,5 +1,18 @@
 package com.thelocalmarketplace.software.receipt;
-
+/*
+ * 
+ * Project iteration 2 group members:
+ * Aj Sallh : 30023811
+ * Anthony Kostal-Vazquez : 30048301
+ * Chloe Robitaille : 30022887
+ * Dvij Raval : 30024340
+ * Emily Kiddle : 30122331
+ * Katelan NG : 30144672
+ * Kingsley Zhong : 30197260
+ * Nick McCamis : 30192610
+ * Sua Lim : 30177039
+ * Subeg CHAHAL : 30196531
+ */
 public interface PrintReceiptListener {
 
 	/**
