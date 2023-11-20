0ab4a972f8dbe0f5da90276e38a1b2416e8f9407
SubegSC <subegschahal@gmail.com>
2023-11-20 02:25:59 -0700

    Updated test cases for unregistering listeners

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/FundsTest_Bronze.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/FundsTest_Bronze.java
@@ -116,7 +116,6 @@ public class FundsTest_Bronze {
 
 	@Test
 	public void testAmountPaidFullCash() throws DisabledException, CashOverloadException, NoCashAvailableException {
-		
 		Currency currency = Currency.getInstance(Locale.CANADA);
 		Coin coinAmountPaid = new Coin(currency, amountPaid);
 
@@ -160,28 +159,44 @@ public class FundsTest_Bronze {
 	}
 
 	@Test
-	public void testUnregisterListener() {
-//		FundsListenerStub stub = new FundsListenerStub();
-//		funds.register(stub);
-//		funds.deregister(stub);
-//		funds.update(price);
-//		value = new BigDecimal(5);
-//		fund.new InnerListener().validCoinDetected(validator, value);
-//		assertFalse("Paid event not called", stub.getEvents().contains("Paid"));
+	public void testUnregisterListener() throws DisabledException, CashOverloadException {
+		Currency currency = Currency.getInstance(Locale.CANADA);
+		Coin coinAmountPaid = new Coin(currency, amountPaid);
+
+		FundsListenerStub stub = new FundsListenerStub();
+		funds.register(stub);
+		funds.deregister(stub);
+		funds.update(price);
+		
+		SessionSimulation sampleSimulation = new SessionSimulation();
+		sampleSimulation.setPayByCash();
+		
+		scs.coinSlot.receive(coinAmountPaid);
+
+		assertFalse("Paid event should not be called", stub.getEvents().contains("Paid"));
 	}
 
 	@Test
-	public void testUnregisterAllListeners() {
-//		FundListenerStub stub = new FundListenerStub();
-//		FundListenerStub stub2 = new FundListenerStub();
-//		fund.register(stub);
-//		fund.register(stub2);
-//		fund.deregisterAll();
-//		fund.update(price);
-//		value = new BigDecimal(5);
-//		fund.new InnerListener().validCoinDetected(validator, value);
-//		assertFalse("Paid event called", stub.getEvents().contains("Paid"));
-//		assertFalse("Paid event called", stub2.getEvents().contains("Paid"));
+	public void testUnregisterAllListeners() throws DisabledException, CashOverloadException {
+		Currency currency = Currency.getInstance(Locale.CANADA);
+		Coin coinAmountPaid = new Coin(currency, amountPaid);
+
+		FundsListenerStub stub = new FundsListenerStub();
+		FundsListenerStub stub2 = new FundsListenerStub();
+
+		funds.register(stub);
+		funds.register(stub2);
+
+		funds.deregisterAll();
+		funds.update(price);
+		
+		SessionSimulation sampleSimulation = new SessionSimulation();
+		sampleSimulation.setPayByCash();
+		
+		scs.coinSlot.receive(coinAmountPaid);
+
+		assertFalse("Paid event should not be called", stub.getEvents().contains("Paid"));
+		assertFalse("Paid event should not be called", stub2.getEvents().contains("Paid"));
 	}
 
 	@Test
