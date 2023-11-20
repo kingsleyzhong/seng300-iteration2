fb80ed12cbb58845cfa98ed32207be8d17e8dd21
Kingsley Zhong <kingsleyzhonghome@gmail.com>
2023-11-20 00:09:26 -0700

    upgrade test suite to be comprehensive

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/TestSuite.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/TestSuite.java
@@ -21,15 +21,24 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-	SelfCheckoutStationSystemTest.class,
-	SelfCheckoutStationLogicTest.class,
-	SessionTest.class,
-	ItemAddedRuleTest.class,
+	AddBagsTest_Bronze.class,
+	AddBagsTest_Gold.class,
+	AddBagsTest_Silver.class,
+	AddBulkyItemTest.class,
 	FundsTest_Bronze.class,
-	FundsTest_Silver.class,
 	FundsTest_Gold.class,
+	FundsTest_Silver.class,
+	ItemAddedRuleTest.class,
+	PayByCardTest.class,
+	PayByCashControllerTest.class,
+	PrintReceiptTest_Bronze.class,
+	PrintReceiptTest_Gold.class,
+	PrintReceiptTest_Silver.class,
+	RemoveItemTests.class,
+	SelfCheckoutStationLogicTest.class,
+	SelfCheckoutStationSystemTest.class,
+	SessionTest.class,
 	WeightTest.class,
-	PayByCardTest.class
 })
 
 public class TestSuite {
